1bddb70a580cb934bcbe2aca0f0a0387b940a8e4
Talon <talonwg27@ausdk12.org>
2025-08-22 13:45:11 -0700

    hw 1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,31 @@
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
+
+    public BodyOfWater(String n, int diameter, double depth, boolean salt){
+        name = n;
+        largestDiameter = diameter;
+        avgDepth = depth;
+        isSaltWater = salt;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setlargestDiameter(int diameter){
+        largestDiameter = diameter;
+    }
+
+      public void setavgDepth(String depth){
+        avgDepth = depth;
+    }
+
+    public void setisSaltWater(int salt){
+        isSaltWater = salt;
+    }
+
+}
 
